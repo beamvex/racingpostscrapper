7a883d675c0b7ffcfea5a7ba5b7b58ebb979a02d
Robert Forster <robert.forster@beamvex.co.uk>
2026-06-12 16:36:27 +0100

    add terraform configuration with github actions workflow for aws deployment using oidc

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "robterraform"
+    key    = "racingpost-scraper/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
