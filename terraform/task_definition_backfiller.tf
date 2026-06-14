e31253f513cbba48392b8485f27f6ed89f6e7a91
Robert Forster <robert.forster@beamvex.co.uk>
2026-06-14 10:34:48 +0100

    increase backfiller task memory from 256 to 512 mb for improved performance

--- terraform/task_definition_backfiller.tf
+++ terraform/task_definition_backfiller.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_task_definition" "backfiller" {
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
-  memory                   = "256"
+  memory                   = "512"
 
   execution_role_arn = aws_iam_role.ecs_task_execution.arn
   task_role_arn      = aws_iam_role.ecs_task.arn
