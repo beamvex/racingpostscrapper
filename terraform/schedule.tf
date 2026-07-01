28f437b8a979be901be01b5f9c815088f9f426f2
Robert Forster <robert.forster@beamvex.co.uk>
2026-07-01 11:17:53 +0100

    remove nightly eventbridge schedule rule, target, and output resources

--- terraform/schedule.tf
+++ terraform/schedule.tf
@@ -1,32 +1,7 @@
-resource "aws_cloudwatch_event_rule" "nightly" {
-  name                = "racingpost-scraper-nightly"
-  description         = "Run racingpost scraper nightly at 22:00 UTC"
-  schedule_expression = "cron(0 22 * * ? *)"
-}
-
 resource "aws_cloudwatch_event_rule" "racecard_daily" {
   name                = "racingpost-racecard-scraper-daily"
   description         = "Run racingpost racecard scraper daily at 09:00 UTC"
   schedule_expression = "cron(0 9 * * ? *)"
-}
-
-resource "aws_cloudwatch_event_target" "nightly" {
-  rule      = aws_cloudwatch_event_rule.nightly.name
-  target_id = "ecs-run-task"
-  arn       = aws_ecs_cluster.main.arn
-  role_arn  = aws_iam_role.eventbridge_ecs_run_task.arn
-
-  ecs_target {
-    task_definition_arn = aws_ecs_task_definition.scraper.arn
-    launch_type         = "FARGATE"
-    platform_version    = "LATEST"
-
-    network_configuration {
-      subnets          = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-      security_groups  = [aws_security_group.ecs_tasks.id]
-      assign_public_ip = true
-    }
-  }
 }
 
 resource "aws_cloudwatch_event_target" "racecard_daily" {
@@ -46,10 +21,6 @@ resource "aws_cloudwatch_event_target" "racecard_daily" {
       assign_public_ip = true
     }
   }
-}
-
-output "nightly_schedule_rule_name" {
-  value = aws_cloudwatch_event_rule.nightly.name
 }
 
 output "racecard_daily_schedule_rule_name" {
