a7ea765a54ae7890087c2722f9cbb471c84dd92a
Robert Forster <robert.forster@beamvex.co.uk>
2026-07-01 09:20:39 +0100

    align lambda function resource attribute indentation in terraform configuration

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -50,9 +50,9 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "probabilities" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "racingpost-probabilities"
-  role            = aws_iam_role.lambda.arn
-  handler         = "lambda_function.lambda_handler"
-  runtime         = "python3.11"
+  role             = aws_iam_role.lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.11"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
