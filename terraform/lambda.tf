549484cb49daaec7c61a343888cd6378a33e66c0
Robert Forster <robert.forster@beamvex.co.uk>
2026-07-01 11:07:41 +0100

    consolidate lambda iam resources from lambda_iam.tf into lambda.tf and remove separate lambda_iam.tf file

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,5 +1,44 @@
-data "aws_iam_role" "lambda" {
-  name = "racingpost-scraper-lambda"
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "racingpost-scraper-lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy_document" "lambda_s3" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.scraper_data.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_s3" {
+  name   = "racingpost-probabilities-s3"
+  role   = aws_iam_role.lambda.id
+  policy = data.aws_iam_policy_document.lambda_s3.json
 }
 
 data "archive_file" "lambda_zip" {
