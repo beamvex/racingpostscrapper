0c284fde60e44162b4e91aca8a4442c573e39325
Robert Forster <robert.forster@beamvex.co.uk>
2026-07-24 17:05:36 +0100

    add lifecycle prevent_destroy rule to S3 data bucket to protect against accidental deletion of scraper data bucket

--- terraform/s3_data_bucket.tf
+++ terraform/s3_data_bucket.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "scraper_data" {
   bucket_prefix = "racingpost-scraper-data-"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "scraper_data" {
